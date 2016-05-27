4b530e85c346735a38c252413781085395a9f8cc
IsNull <p.buettiker@elderbyte.com>
2016-05-27 15:20:58 +0200

    support eureka service discovery

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: java -Dspring.profiles.active=cloud -jar  sample-server/build/libs/sample-server-0.1.0.jar
+web: java -Dspring.profiles.active=cloud -jar sample-server/build/libs/sample-server-0.1.0.jar
 
--- sample-server/src/main/java/com/elderbyte/paassample/SampleApplicaitonServer.java
+++ sample-server/src/main/java/com/elderbyte/paassample/SampleApplicaitonServer.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.ApplicationContext;
 import org.springframework.core.env.Environment;
 
@@ -14,6 +15,7 @@ import java.net.UnknownHostException;
  * Entry point of spring boot app
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class SampleApplicaitonServer {
 
     private static final Logger log = LoggerFactory.getLogger(SampleApplicaitonServer.class);
